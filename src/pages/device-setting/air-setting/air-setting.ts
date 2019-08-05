7f19ca9f9ad0187a850fadb7d18b2440f3015cee
周波 <zhoubo@zhoubodeMBP.mshome.net>
2019-08-05 13:18:28 +0800

    bf

--- src/pages/device-setting/air-setting/air-setting.ts
+++ src/pages/device-setting/air-setting/air-setting.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, Events } from 'ionic-angular';
 import * as ProgressBar from "progressbar.js";
 import { Variable } from '../../../providers/model/variable';
+import { DeviceRequestsProvider } from '../../../providers/tools/requests'
+import { ToolsProvider } from '../../../providers/tools/tools'
 
 /**
  * Generated class for the AirSettingPage page.
@@ -16,34 +18,41 @@ import { Variable } from '../../../providers/model/variable';
   templateUrl: 'air-setting.html',
 })
 export class AirSettingPage {
+
   name: string;
-  id: string;
+  id: number;
   paramData: any = {};
-  temp: number = 25;
-  private tempMax: number;
-  private tempMin: number;
+  temp: number = 16;
+  private tempMax: number = 30;
+  private tempMin: number = 16;
   barCircleObj: any;
-  modeColumns: any;
-  speedColumns: any;
-  sleepColumns: any;
-  modeModel: number = 1;
-  speedModel: number;
-  sleepModel: number;
-  modeKV: any = {};
-  speedKV: any = {};
-  spaceTemp: number;
+  modeKV: any = [];
+  speedKV: any = [];
+  tempKv: any = [];
 
   open: boolean;
-  sleep: number;
   // speed: number;
   airData: any = {};
   private monitorID: number;
   private deviceID: number;
   tempColumns: any = [];
 
+  airParams: any;
+  paramsData: any;
+  roomTempData: any;
+  // settingTempData: any;
+  selectedMode: any = {};
+  selectedSpped: any = {};
+  openData: any;
+  fnID: number;
+  fnID53: number;
+  setInfo: any = { type: '', value: '' };
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private modalCtrl: ModalController,
-    private events: Events) {
+    private events: Events,
+    private device: DeviceRequestsProvider,
+    private tools: ToolsProvider) {
 
     this.id = this.navParams.get("id");
     this.name = this.navParams.get("name");
@@ -51,83 +60,144 @@ export class AirSettingPage {
     this.monitorID = this.paramData.F_MonitorID;
     this.deviceID = this.paramData.F_ID;
 
-
-
-
-    // this.tempMax = 28;
-    // this.tempMin = 16;
-    this.modeKV = {
-      0: { name: '冷风模式', url: "cool.png", class: 'selected' },
-      1: { name: '暖风模式', url: "nuanfeng.png", class: 'hot' },
-      2: { name: '地暖模式', url: "dinuan.png", class: 'hot' },
-      3: { name: '暖风&地暖', url: "nuanfeng1.png", class: 'hot' },
-      4: { name: '除湿模式', url: "chushi.png", class: 'selected' },
-      5: { name: '通风模式', url: "songfeng.png", class: 'selected' },
-    };
-    this.speedKV = {
-      0: '自动',
-      1: '低速',
-      2: '中速',
-      3: '高速'
-    };
-
+    this.getTempColumns();
 
 
   }
+  getFnData(fnID: number) {
+    let fnData = Variable.GetFnData(fnID + "");
+    this.getParamsFnData(fnData);
+    this.fnID53 = fnID;
+    this.events.subscribe(`FnData:${fnID}`, this.eventsFn53Handler);
+
+  }
+  getParamsFnData(data: any) {
+    this.getDeviceState(data.State);
+    data = data.Data;
+    let code = this.airParams.F_ParamsFnCode;
+    let code1 = this.airParams.F_RoomTempFnCode;
+    let code2 = this.airParams.F_SettingTempFnCode;
+
+    this.paramsData = data[code] ? data[code] : 0;
+    this.roomTempData = data[code1] ? data[code1] : 0;
+    this.temp = data[code2] ? Number(data[code2]) : this.tempMin;
+
+    if (this.setInfo.type === 'temp') { if (this.temp == this.setInfo.value) { this.dismissLoading(); } }
+
+    // this.paramsData = 33928;
+    let str = this.tools.numTo15BitArr(this.paramsData);
+    str.forEach((value, index) => {
+
+      if (value == 1) {
+
+        //是否是开关
+        let find = false;
+        this.modeKV.forEach(element => {
+          if (element.F_Bit == index) {
+            this.selectedMode = element;
+            if (this.setInfo.type === 'mode') { if (element.F_ID == this.setInfo.value) { this.dismissLoading(); } }
+
+            find = true;
+          }
+        });
+        if (find == false) {
+          this.speedKV.forEach(element => {
+            if (element.F_Bit == index) {
+              this.selectedSpped = element;
+              if (this.setInfo.type === 'speed') { if (element.F_ID == this.setInfo.value) { this.dismissLoading(); } }
+
+            }
+          });
+        }
+
+      }
+    });
+
+    this.setCircleNum();
+  }
+  dismissLoading() {
+    this.setInfo.type = '';
+    this.setInfo.value = '';
+    Variable.socketObject.dismissLoading();
+  }
+  checkSetInfo() {
+    switch (this.setInfo.type) {
+      case 'open': { break; }
+      case 'speed': {
+
+        break;
+      }
+      case 'mode': { break; }
+
+    }
+  }
+
   ionViewDidLeave() {
-    this.events.unsubscribe(`AirData:${this.monitorID}`, this.eventsAirHandler);
+    // this.events.unsubscribe(`FnData:${this.fnID}`, this.eventsFn51Handler);
+    this.events.unsubscribe(`FnData:${this.fnID53}`, this.eventsFn53Handler);
   }
 
   ionViewDidLoad() {
-
     this.setCircle();
-    this.airData = Variable.GetAirData(this.monitorID + "");
-    this.setDetailData();
-    this.events.subscribe(`AirData:${this.monitorID}`, this.eventsAirHandler);
 
-    Variable.socketObject.getAirData(this.monitorID);
+
+    this.device.getAirParamsDataList().then(res => {
+      this.modeKV = res['mode'];
+      this.speedKV = res['speed'];
+      this.tempKv = res['temp'];
+      this.device.getAirDataByID(this.id).then(res => {
+        this.airParams = res;
+        this.getFnData(this.airParams["F_FnID"]);
+
+
+      });
+
+    });
+
+    // this.device.getDeviceGetInfoDataByID(this.id).then(res => {
+    //   this.fnID = res["F_FnID"];
+    //   let fnData = Variable.GetFnData(this.fnID.toString());
+    //   this.getDeviceState(fnData);
+    //   this.events.subscribe(`FnData:${this.fnID}`, this.eventsFn51Handler);
+    // });
 
   }
-  setDetailData() {
-    let data = this.airData;
-    if (data.F6010) {
+  // private eventsFn51Handler = (data: any) => {
+  //   this.getDeviceState(data);
+  // }
 
-      this.tempMin = this.getTempNum(data.F6010) ;
-      this.tempMax = this.getTempNum(data.F6011);
-      this.temp = this.getTempNum(data.F602);
-      this.spaceTemp = this.getTempNum(data.F6013);
-      this.open = Boolean(Number(data.F601));
-      this.modeModel = Number(data.F603);
-      this.sleepModel = Number(data.F607);
-      this.speedModel = Number(data.F608);
-      let t = [];
-      for (let i = this.tempMin; i <= this.tempMax; i += 0.5) {
-        t.push({ text: `${i.toFixed(1)}`, value: i });
-      }
-      this.tempColumns = [
-        {
-          options: t
-        }];
-      // this.tempColumns = [
-      //   {
-      //     options: [
-      //       { text: '8.0', value: 8 },
-      //       { text: '1.0', value: 1 },
-      //       { text: '风盘+地冷', value: 2 }
-      //     ]
-      //   }];
-      this.setCircleNum();
+  private eventsFn53Handler = (data: any) => {
+    this.getParamsFnData(data);
+  }
+  getDeviceState(data: any) {
+    if (data) {
+      this.open = data[this.id];
+      if (this.setInfo.type === 'open') { if (this.open == this.setInfo.value) { this.dismissLoading(); } }
 
-
-      // this.setCircle(data.F6010, data.F6011);
     }
   }
+  // setDeviceState(state: any) {
+  //   this.open = state;
+  //   Variable.socketObject.setDeviceState(this.id, this.name, state);
+  // }
+
+  getTempColumns() {
+    let t = [];
+    for (let i = this.tempMin; i <= this.tempMax; i++) {
+      t.push({ text: `${i}`, value: i });
+    }
+    this.tempColumns = [
+      {
+        options: t
+      }];
+  }
+
   private getTempNum(data: string) {
     let num: number = Number(data);
     return num;
   }
   setCircle() {
-    this.barCircleObj = new ProgressBar.Circle(document.getElementById("container"), {
+    this.barCircleObj = new ProgressBar.Circle(document.getElementById("circlebar"), {
       strokeWidth: 3,
       easing: 'easeInOut',
       duration: 500,
@@ -137,6 +207,7 @@ export class AirSettingPage {
       svgStyle: null,
       // boxShadow: '0 2px 6px 0 #4D95DF',
     });
+    this.setCircleNum();
   }
   setCircleNum() {
     let num = (this.temp - this.tempMin) / (this.tempMax - this.tempMin);
@@ -146,69 +217,82 @@ export class AirSettingPage {
   tempAdd() {
     this.temp = Number(this.temp);
     if (this.temp < this.tempMax) {
-      this.temp += 0.5;
+      this.temp++;
       this.setCircleNum();
-      this.sendAir();
+      this.setAirTemp();
+      // this.sendAir();
 
     }
 
   }
-  private sendAir() {
-    Variable.socketObject.setAir(`1,${Number(this.temp)}`, this.deviceID, this.monitorID);
+  private setAirTemp() {
+    this.setAir(this.tempKv[0]['F_Mode'], Number(this.temp) + "");
+    this.setInfo.type = 'temp';
+    this.setInfo.value = this.temp;
+
   }
   tempSub() {
     this.temp = Number(this.temp);
 
     if (this.temp > this.tempMin) {
-      this.temp -= 0.5;
+      this.temp--;
       this.setCircleNum();
-      this.sendAir();
+      this.setAirTemp();
+      // this.sendAir();
     }
   }
   changeTemp() {
     this.setCircleNum();
-    this.sendAir();
+    this.setAirTemp();
 
-  }
-  goMorePage() {
-    this.navCtrl.push("AirSettingMorePage", { id: this.id, name: this.name });
   }
   modeChange() {
   }
 
   setOpen() {
     this.open = !this.open;
-    Variable.socketObject.setAir(`0,${Number(this.open)}`, this.deviceID, this.monitorID);
+    Variable.socketObject.setDeviceState(this.id, this.name, this.open);
+    this.setInfo.type = 'open';
+    this.setInfo.value = this.open;
   }
-  setSpeed(data: number) {
+  setSpeed(data: any) {
     // this.speed = data;
-    this.speedModel = data;
-    Variable.socketObject.setAir(`7,${Number(this.speedModel)}`, this.deviceID, this.monitorID);
+    this.selectedSpped = data;
+    this.setAir(data['F_Mode'], data['F_Code']);
+    this.setInfo.type = 'speed';
+    this.setInfo.value = data.F_ID;
 
   }
-  setSleep() {
-    if (this.sleep > 0) {
-      this.sleep = 0;
-    } else {
-      this.sleep = 1;
-    }
-  }
+
   setMode() {
-    let modalObj = this.modalCtrl.create('AirSettingModePage');
+    let modalObj = this.modalCtrl.create('AirSettingModePage', { Data: this.modeKV });
     modalObj.onDidDismiss(res => {
       if (res != null) {
-        this.modeModel = res;
-        Variable.socketObject.setAir(`2,${Number(this.modeModel)}`, this.deviceID, this.monitorID);
-
+        this.selectedMode = res;
+        this.setAir(res['F_Mode'], res['F_Code']);
+        this.setInfo.type = 'mode';
+        this.setInfo.value = res.F_ID;
       }
     });
     modalObj.present();
 
 
   }
-  private eventsAirHandler = (data: any) => {
-    this.airData = data;
-    this.setDetailData();
-  }
 
+  setAir(mode: string, code: string) {
+    let setID = this.airParams.F_SetID;
+
+    let data = `${setID},${mode},${code}`;//42 主机标号 模式 参数
+
+
+    Variable.socketObject.setAir(data, this.airParams.F_MonitorID, this.airParams.F_SetFnID);
+  }
+  // private eventsAirHandler = (data: any) => {
+  //   this.airData = data;
+  //   this.setDetailData();
+  // }
+
+  goMorePage() {
+    this.navCtrl.push("AirSettingMorePage", { id: this.id, name: this.name });
+  }
 }
